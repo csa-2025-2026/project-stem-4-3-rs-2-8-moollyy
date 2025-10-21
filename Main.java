bff71b75693fe26c5128efbbc2bb039b11b3696c
moollyy <136740176+moollyy@users.noreply.github.com>
2025-10-21 18:22:34 +0000

    2

--- Main.java
+++ Main.java
@@ -5,5 +5,17 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+
+    // prob 1
+    int num = 1; 
+    for (int count = 1; count <= 25; count+=2) // in the loop when count is leess than 25
+    {
+      System.out.println(count);
+    }
+
   }
+
+  //prob 2
+  for (int count = 17; count <= 73; count++)
+
 }
