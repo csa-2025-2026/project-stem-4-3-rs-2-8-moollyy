317bc0668f2ed41d65a181080b2cc5fb473f182c
moollyy <136740176+moollyy@users.noreply.github.com>
2025-10-23 17:49:20 +0000

    :I

--- Main.java
+++ Main.java
@@ -15,7 +15,47 @@ public class Main
 
   }
 
-  //prob 2
-  for (int count = 17; count <= 73; count++)
+  // //prob 2
+
+  // int counter = 1;
+  // for (int i = 17; i <= 73; i++) 
+  // {
+  //   System.out.print(i + " ");
+
+  //   if (counter == 10)
+  //   {
+  //     System.out.println();
+  //     counter = 0; 
+
+  //   }
+
+  //   counter++;
+  // }
+
+  // // prob 
+  // int num = 0;
+  // for (int i = 0; i <= 50; i++)
+  // {
+  //   System.out.println(i + " ");
+
+  //   if counter (counter == 5)
+  //   {
+  //     System.out.println();
+  //     counter = 0; 
+  //   }
+  // }
+
+  int user_input = 23;
+  int counter = 1; 
+  for (int i = user_input; i <= 50; i++)
+  {
+    System.out.print(i + " ");
+    if (counter == 5)
+    {
+      counter = 0;
+      System.out.println();
+    }
+    counter++;
+  }
 
 }
